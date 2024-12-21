7e95def6530074df58b733aedfa630e1e5a3f020
Velauther Vijayaruban <vvijayarub@pattonair.com>
2024-12-21 00:31:07 +0000

    update utility folder

---
+++ utility/Usage/Usage/Models.cs
@@ -0,0 +1,541 @@
+﻿namespace Usage
+{
+    using System.Text.Json.Serialization;
+
+    // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+    public class _012023
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _012024
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _022023
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _022024
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _032023
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _032024
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _042023
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _042024
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _052023
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _052024
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _062023
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _062024
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _072023
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _072024
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _082023
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _082024
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _092023
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _092024
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _102023
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _102024
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _112023
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _112024
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _122022
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _122023
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class _122024
+    {
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("partitions")]
+        public Partitions Partitions { get; set; }
+
+        [JsonPropertyName("amountSaved")]
+        public double? AmountSaved { get; set; }
+
+        [JsonPropertyName("amountSavedUsagePercentage")]
+        public double? AmountSavedUsagePercentage { get; set; }
+    }
+
+    public class ALL
+    {
+        [JsonPropertyName("Values")]
+        public List<string> Values { get; set; }
+
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("totals")]
+        public Totals Totals { get; set; }
+
+        [JsonPropertyName("Quality")]
+        public string Quality { get; set; }
+
+        [JsonPropertyName("Units")]
+        public double? Units { get; set; }
+
+        [JsonPropertyName("Cost")]
+        public double? Cost { get; set; }
+    }
+
+    public class Detail
+    {
+        [JsonPropertyName("StartDate")]
+        public DateTime? StartDate { get; set; }
+
+        [JsonPropertyName("EndDate")]
+        public DateTime? EndDate { get; set; }
+
+        [JsonPropertyName("Days")]
+        public int? Days { get; set; }
+
+        [JsonPropertyName("Rate")]
+        public double? Rate { get; set; }
+
+        [JsonPropertyName("Total")]
+        public double? Total { get; set; }
+    }
+
+    public class FixedCosts
+    {
+        [JsonPropertyName("details")]
+        public List<Detail> Details { get; set; }
+
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+    }
+
+    public class Months
+    {
+        [JsonPropertyName("12-2022")]
+        public _122022 _122022 { get; set; }
+
+        [JsonPropertyName("01-2023")]
+        public _012023 _012023 { get; set; }
+
+        [JsonPropertyName("02-2023")]
+        public _022023 _022023 { get; set; }
+
+        [JsonPropertyName("03-2023")]
+        public _032023 _032023 { get; set; }
+
+        [JsonPropertyName("04-2023")]
+        public _042023 _042023 { get; set; }
+
+        [JsonPropertyName("05-2023")]
+        public _052023 _052023 { get; set; }
+
+        [JsonPropertyName("06-2023")]
+        public _062023 _062023 { get; set; }
+
+        [JsonPropertyName("07-2023")]
+        public _072023 _072023 { get; set; }
+
+        [JsonPropertyName("08-2023")]
+        public _082023 _082023 { get; set; }
+
+        [JsonPropertyName("09-2023")]
+        public _092023 _092023 { get; set; }
+
+        [JsonPropertyName("10-2023")]
+        public _102023 _102023 { get; set; }
+
+        [JsonPropertyName("11-2023")]
+        public _112023 _112023 { get; set; }
+
+        [JsonPropertyName("12-2023")]
+        public _122023 _122023 { get; set; }
+
+        [JsonPropertyName("01-2024")]
+        public _012024 _012024 { get; set; }
+
+        [JsonPropertyName("02-2024")]
+        public _022024 _022024 { get; set; }
+
+        [JsonPropertyName("03-2024")]
+        public _032024 _032024 { get; set; }
+
+        [JsonPropertyName("04-2024")]
+        public _042024 _042024 { get; set; }
+
+        [JsonPropertyName("05-2024")]
+        public _052024 _052024 { get; set; }
+
+        [JsonPropertyName("06-2024")]
+        public _062024 _062024 { get; set; }
+
+        [JsonPropertyName("07-2024")]
+        public _072024 _072024 { get; set; }
+
+        [JsonPropertyName("08-2024")]
+        public _082024 _082024 { get; set; }
+
+        [JsonPropertyName("09-2024")]
+        public _092024 _092024 { get; set; }
+
+        [JsonPropertyName("10-2024")]
+        public _102024 _102024 { get; set; }
+
+        [JsonPropertyName("11-2024")]
+        public _112024 _112024 { get; set; }
+
+        [JsonPropertyName("12-2024")]
+        public _122024 _122024 { get; set; }
+    }
+
+    public class Partitions
+    {
+        [JsonPropertyName("ALL")]
+        public ALL ALL { get; set; }
+    }
+
+    public class PartitionTypes
+    {
+        [JsonPropertyName("ALL")]
+        public ALL ALL { get; set; }
+    }
+
+    public class Root
+    {
+        [JsonPropertyName("partitionTypes")]
+        public PartitionTypes PartitionTypes { get; set; }
+
+        [JsonPropertyName("total")]
+        public double? Total { get; set; }
+
+        [JsonPropertyName("months")]
+        public Months Months { get; set; }
+
+        [JsonPropertyName("fixedCosts")]
+        public FixedCosts FixedCosts { get; set; }
+    }
+
+    public class Totals
+    {
+        [JsonPropertyName("ALL")]
+        public ALL ALL { get; set; }
+    }
+
+
+}
